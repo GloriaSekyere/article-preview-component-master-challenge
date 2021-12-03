d701faac12a4552e5b9f3ae566d2a740f043e9c8
GloriaSekyere <gloriasekyere15@gmail.com>
2021-12-03 08:27:19 +0000

    begin desktop view

--- index.js
+++ index.js
@@ -6,43 +6,59 @@ let activeBlock = document.getElementById("wrapper-active");
 
 let isActive = false;
 
-(function transition() {
-  inactiveBlock.style.transition = "all 1s";
-  activeBlock.style.transition = "all 1s";
-})();
-
-function makeInactive() {
-  activeBlock.style.display = "none";
-  inactiveBlock.style.display = "block";
-}
-
-function makeActive() {
-  inactiveBlock.style.display = "none";
-  activeBlock.style.display = "block";
-}
-
-function display() {
-  if (isActive == false) {
-    activeBlock.style.opacity = "0";
-    inactiveBlock.style.opacity = "1";
-    setTimeout(makeInactive, 750);
+function myFunction(mediaQuery) {
+  if (mediaQuery.matches) { // If media query matches
+    //document.body.style.backgroundColor = "yellow";
+    activeBlock.style.display = "none";
   } 
   else {
-    inactiveBlock.style.opacity = "0";
-    activeBlock.style.opacity = "1";
-    setTimeout(makeActive, 750);
+    //document.body.style.backgroundColor = "pink";
+
+      (function transition() {
+        inactiveBlock.style.transition = "all 1s";
+        activeBlock.style.transition = "all 1s";
+      })();
+
+      function makeInactive() {
+        activeBlock.style.display = "none";
+        inactiveBlock.style.display = "block";
+      }
+
+      function makeActive() {
+        inactiveBlock.style.display = "none";
+        activeBlock.style.display = "block";
+      }
+
+      function display() {
+        if (isActive == false) {
+          activeBlock.style.opacity = "0";
+          inactiveBlock.style.opacity = "1";
+          setTimeout(makeInactive, 750);
+        } 
+        else {
+          inactiveBlock.style.opacity = "0";
+          activeBlock.style.opacity = "1";
+          setTimeout(makeActive, 750);
+        }
+      }
+
+      inactiveIcon.onclick = function() {
+        isActive = true;
+        display();
+      }
+
+      activeIcon.onclick = function() {
+        isActive = false;
+        display();
+    }
   }
 }
 
-inactiveIcon.onclick = function() {
-  isActive = true;
-  display();
-}
+let mediaQuery = window.matchMedia("(min-width: 768px)")
+myFunction(mediaQuery) // Call listener function at run time
+mediaQuery.addListener(myFunction) // Attach listener function on state changes
 
-activeIcon.onclick = function() {
-  isActive = false;
-  display();
-}
+
 
 
 
