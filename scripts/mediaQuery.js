5315a55eb24474915e715a07ab523fc4af45993d
GloriaSekyere <gloriasekyere15@gmail.com>
2022-01-25 20:12:32 +0000

    complete active state desktop

--- scripts/mediaQuery.js
+++ scripts/mediaQuery.js
@@ -1,27 +1,31 @@
 const mediaQuery = window.matchMedia("(min-width: 768px)")
 
 let activeDesktop = document.getElementById("wrapper-active-desktop");
+let destopShare = document.getElementById("share");
 
 function desktopDisplay() {
   if (isActive == false) {
-    activeDesktop.style.visibility = "visible";
     activeDesktop.style.opacity = "1";
+    destopShare.setAttribute("style", "fill: #FFF;");
+    inactiveIcon.style.backgroundColor = "hsl(214, 17%, 51%)";
   } 
   else {
     activeDesktop.style.opacity = "0";
-    activeDesktop.style.visibility = "hidden";
+    destopShare.setAttribute("style", "fill: hsl(214, 17%, 51%);");
+    inactiveIcon.style.backgroundColor = "hsl(210, 46%, 95%)";
   } 
 }
 
 function myFunction(mediaQuery) {
   if (mediaQuery.matches) { // If media query matches
     inactiveIcon.onclick = function() {
-      isActive  = (isActive == false) ? true : false;
       activeDesktop.style.transition = "all 0.5s";
       desktopDisplay();
+      isActive  = (isActive == false) ? true : false;
     }
   } 
   else {
+    
     inactiveIcon.onclick = function() {
       isActive = true;
       inactiveBlock.style.transition = "all 1s";
